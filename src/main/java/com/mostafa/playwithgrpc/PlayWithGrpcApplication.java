81ae5dddbb14141b4ce0139da356347e4f54b29c
m0stafa_darwe4 <mostafadarwesh610@gmail.com>
2024-01-07 21:26:17 +0200

    add: project structure

---
+++ src/main/java/com/mostafa/playwithgrpc/PlayWithGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.mostafa.playwithgrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class PlayWithGrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayWithGrpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mostafa/playwithgrpc/PlayWithGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mostafa.playwithgrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayWithGrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

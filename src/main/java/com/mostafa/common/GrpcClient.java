ac5e3885007b627278ef7450b213f1d5e154707d
m0stafa_darwe4 <mostafadarwesh610@gmail.com>
2024-07-01 01:36:55 +0300

    add async stub

--- src/main/java/com/mostafa/common/GrpcClient.java
+++ src/main/java/com/mostafa/common/GrpcClient.java
@@ -1,22 +1,41 @@
 package com.mostafa.common;
 
+import com.mostafa.models.sec06.AccountBalance;
 import com.mostafa.models.sec06.BalanceCheckRequest;
 import com.mostafa.models.sec06.BankServiceGrpc;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.time.Duration;
 
 public class GrpcClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         var channel = ManagedChannelBuilder.forAddress("localhost", 6565)
                 .usePlaintext()
                 .build();
 
 
-        var stab = BankServiceGrpc.newBlockingStub(channel);
+        var stub = BankServiceGrpc.newStub(channel);
 
-        var accountBalance = stab.getAccountBalance(BalanceCheckRequest.newBuilder()
-                .setAccountNumber(2).build());
+        stub.getAccountBalance(BalanceCheckRequest.newBuilder().setAccountNumber(2).build(),
+                new StreamObserver<>() {
+            @Override
+            public void onNext(AccountBalance accountBalance) {
+                System.out.println(accountBalance);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("completed");
+            }
+        });
 
 
-        System.out.println("accountBalance = " + accountBalance);
+        Thread.sleep(1000);
+
     }
 }

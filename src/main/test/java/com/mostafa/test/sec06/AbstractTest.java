d01ef4764b80715b97303678e9af84c21c0f13c7
m0stafa_darwe4 <mostafadarwesh610@gmail.com>
2024-07-16 21:43:47 +0300

    Add testing server classes

---
+++ src/main/test/java/com/mostafa/test/sec06/AbstractTest.java
@@ -0,0 +1,29 @@
+package com.mostafa.test.sec06;
+
+import com.mostafa.common.GrpcServer;
+import com.mostafa.models.sec06.BankServiceGrpc;
+import com.mostafa.service.BankService;
+import com.mostafa.test.common.AbstractChannelTest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class AbstractTest extends AbstractChannelTest {
+
+    private final GrpcServer grpcServer = GrpcServer.create(new BankService());
+
+    protected BankServiceGrpc.BankServiceBlockingStub blockingStub;
+
+    @BeforeAll
+    public void setup() {
+        this.grpcServer.start();
+        this.blockingStub = BankServiceGrpc.newBlockingStub(channel);
+    }
+
+
+    @AfterAll
+    public void stop(){
+        this.grpcServer.stop();
+    }
+}

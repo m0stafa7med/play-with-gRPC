11a869a6d1aa6f2c559ffbffa3e8c78d30183b0a
m0stafa_darwe4 <mostafadarwesh610@gmail.com>
2024-07-23 11:23:28 +0300

    Add get all accounts function

--- src/main/java/com/mostafa/repository/AccountRepository.java
+++ src/main/java/com/mostafa/repository/AccountRepository.java
@@ -1,5 +1,6 @@
 package com.mostafa.repository;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -8,12 +9,16 @@ import java.util.stream.IntStream;
 public class AccountRepository {
     public static final Map<Integer, Integer> db = IntStream.rangeClosed(1, 10)
             .boxed()
-            .collect(Collectors.toConcurrentMap(Function.identity(), v -> 100));
+            .collect(Collectors.toMap(Function.identity(), v -> 100));
+
 
 
     public static Integer getBalance(int accountNumber) {
+        System.out.println(db);
         return db.get(accountNumber);
     }
 
-
+    public static Map<Integer, Integer> getAllAccounts() {
+        return Collections.unmodifiableMap(db);
+    }
 }
--- src/main/java/com/mostafa/service/BankService.java
+++ src/main/java/com/mostafa/service/BankService.java
@@ -1,11 +1,16 @@
 package com.mostafa.service;
 
 
+import com.google.protobuf.Empty;
 import com.mostafa.models.sec06.AccountBalance;
+import com.mostafa.models.sec06.AllAccountsResponse;
 import com.mostafa.models.sec06.BalanceCheckRequest;
 import com.mostafa.models.sec06.BankServiceGrpc;
 import com.mostafa.repository.AccountRepository;
 import io.grpc.stub.StreamObserver;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
 
@@ -14,12 +19,28 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
                                   StreamObserver<AccountBalance> responseObserver) {
 
         int accountNumber = request.getAccountNumber();
-        var account = AccountRepository.getBalance(accountNumber);
+        var balance = AccountRepository.getBalance(accountNumber);
         var accountBalance = AccountBalance.newBuilder()
                 .setAccountNumber(accountNumber)
-                .setBalance(accountNumber * 10).build();
+                .setBalance(accountNumber * 10 + balance).build();
 
         responseObserver.onNext(accountBalance);
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getAllAccounts(Empty request, StreamObserver<AllAccountsResponse> responseObserver) {
+        var accounts = AccountRepository.getAllAccounts()
+                .entrySet()
+                .stream()
+                .map(e -> AccountBalance.newBuilder()
+                        .setAccountNumber(e.getKey()).setBalance(e.getValue()).build())
+                .collect(Collectors.toList());
+
+        var response = AllAccountsResponse.newBuilder().addAllAccounts(accounts).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
     }
 }
